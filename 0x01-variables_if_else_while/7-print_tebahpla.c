76476442c4a27a225d2012e1aaaa88cf6d19a5fb
Ajoe62 <ijeborjoe@gmail.com>
2023-07-14 19:13:03 -0700

    Variable if, else,while

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entering point
+ * Description: Print the alphabet in reverse order in lowercase.
+ * Return: 0
+ */
+
+int main(void)
+{
+	char c;
+
+	for (c = 'z'; c >= 'a'; c--)
+	{
+		putchar(c);
+	}
+	putchar('\n');
+	return (0);
+}
