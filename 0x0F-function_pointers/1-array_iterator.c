78467ff0ec8692ee8640d5848ebfcab21299f00a
Ajoe62 <ijeborjoe@gmail.com>
2023-08-16 13:27:24 -0700

    mandatory tasks

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include "function_pointers.h"
+
+/**
+ * array_iterator - This executes a function given as a parameter
+ * on each element of an array
+ * @array: array to iterate over
+ * @size: size of the array
+ * @action: pointer to function
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	unsigned int a;
+
+	if (!array || !action)
+		return;
+
+	for (a = 0; a < size; a++)
+		action(array[a]);
+}
---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,27 @@
+#include "function_pointers.h"
+
+/**
+ * int_index - searches for an integer
+ * @array: array to search in
+ * @size: size of the array
+ * @cmp: pointer to the comparing function
+ *
+ * Return: index of the first element for which
+ * the cmp function does not return 0, or -1 if no match is found
+ * or size is negative
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int a;
+
+	if (array && cmp)
+	{
+		for (a = 0; a < size; a++)
+		{
+			if (cmp(array[a]) != 0)
+				return (a);
+		}
+	}
+
+	return (-1);
+}
